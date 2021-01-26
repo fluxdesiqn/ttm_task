314722ae1431ef65c789d51bc97eb3bbe39f62c6
Connor Shaw <connor6shaw@gmail.com>
2021-01-26 22:42:22 +0000

    updating the header

--- ttm-base/header.php
+++ ttm-base/header.php
@@ -24,14 +24,13 @@ $container = get_theme_mod('ttm_container_type');
 <body <?php body_class(); ?> <?php ttm_body_attributes(); ?>>
 <?php do_action('wp_body_open'); ?>
 <div class="site" id="page">
-	<header id="main-header">
+	<header id="main-header c-header">
 		<!-- ******************* The Navbar Area ******************* -->
 		<nav id="brand-nav" class="navbar navbar-expand-lg" aria-labelledby="main-nav-label">
 			<?php if ('container' === $container) { ?>
 			<div class="container">
 				<?php } ?>
-
-				<div class="logo-wrapper">
+				<div class="logo-wrapper c-header-logo">
 					<!-- Your site title as branding in the menu -->
 					<?php if (!has_custom_logo()) { ?>
 
@@ -55,32 +54,49 @@ $container = get_theme_mod('ttm_container_type');
 					}
 					?>
 				</div><!-- end custom logo -->
-				<div class="navbar-wrapper">
-					<!-- The WordPress Menu goes here -->
-					<?php wp_nav_menu(
-							[
-									'theme_location' => 'top',
-									'menu_class' => 'navbar-nav nav-fill',
-									'container_class' => 'collapse navbar-collapse',
-									'container_id' => 'topNav',
-									'fallback_cb' => '',
-									'menu_id' => 'top-menu',
-									'depth' => 1,
-									'walker' => new TTM_WP_Bootstrap_Navwalker(),
-							]
-					); ?>
+				<div class="c-header-navigation d-lg-flex justify-content-lg-end d-none">
+					<div class="navbar-wrapper c-header-navigation--desktop mr-2">
+						<!-- The WordPress Menu goes here -->
+						<?php wp_nav_menu(
+								[
+										'theme_location' => 'top',
+										'menu_class' => 'navbar-nav nav-fill',
+										'container_class' => 'collapse navbar-collapse',
+										'container_id' => 'topNav',
+										'fallback_cb' => '',
+										'menu_id' => 'top-menu',
+										'depth' => 1,
+										'walker' => new TTM_WP_Bootstrap_Navwalker(),
+								]
+						); ?>
+					</div>
+					<div class="c-header-call-back js-nav-call-back mr-2">
+						<span>Request a call back</span>
+					</div>
+					<div class="c-header-search js-nav-search">
+						<span><img src="/wp-content/uploads/2021/01/search-line.png" alt="search icon"></span>
+					</div>
 				</div>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNav"
-						aria-controls="mainNav" aria-expanded="false"
-						aria-label="<?php esc_attr_e('Toggle navigation', '_themename'); ?>">
-					Toggle navigation
-				</button>
+				<div class="c-header-navigation--mobile d-flex d-lg-none">
+					<div class="c-header-call-back--mobile js-nav-call-back--mobile">
+						<a href="tel:01302123456"><img src="/wp-content/uploads/2021/01/phone-fill.png" alt="phone icon"></a>
+					</div>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNav"
+							aria-controls="mainNav" aria-expanded="false"
+							aria-label="<?php esc_attr_e('Toggle navigation', '_themename'); ?>">
+						<img src="/wp-content/uploads/2021/01/menu-3-fill.png" alt="menu icon">
+					</button>
+				</div>
 				<?php if ('container' === $container) { ?>
 
 			</div><!-- .container -->
 		<?php } ?>
 		</nav><!-- .site-navigation -->
-		<nav id="main-nav" class="navbar navbar-expand-lg navbar-dark bg-primary" aria-labelledby="main-nav-label">
+		<div class="c-header-search--mobile bg-primary w-100 d-inline-flex d-lg-none">
+			<input type="text" class="js-nav-search--mobile" name="search" placeholder="Search...">
+			<img class="ml-auto" src="/wp-content/uploads/2021/01/search-line-1.png" alt="search icon">
+		</div>
+		<nav id="main-nav" class="navbar navbar-expand-lg navbar-dark bg-primary c-header-menu" aria-labelledby="main-nav-label">
 			<?php if ('container' === $container) { ?>
 			<div class="container"><!-- .container -->
 				<?php } ?>
